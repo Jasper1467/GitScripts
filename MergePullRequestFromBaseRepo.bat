f9a94653128559f7c1e9e8a209016867f17c50aa
Jasper1467 <Jasper1467@proton.me>
2023-06-18 21:27:48 +0200

    MergePullRequestFromBaseRepo update

--- MergePullRequestFromBaseRepo.bat
+++ MergePullRequestFromBaseRepo.bat
@@ -1,32 +1,32 @@
-#!/bin/bash
+@echo off
 
-# Prompt the user for the original repository URL and pull request number
-read -p "Enter the URL of the original repository: " original_repo_url
-read -p "Enter the pull request number: " pull_request_number
+set /p original_repo_url="Enter the URL of the original repository: "
+set /p pull_request_number="Enter the pull request number: "
 
-# Step 1: Ensure your fork is up to date
-git remote add upstream "$original_repo_url"
+REM Step 1: Ensure your fork is up to date
+git remote add upstream "%original_repo_url%"
 git fetch upstream
 git checkout main
 git merge upstream/main
 
-# Step 2: Apply the changes locally
+REM Step 2: Apply the changes locally
 git checkout main
 git pull origin main
 git checkout -b pr-branch
-git pull "$original_repo_url" "pull/$pull_request_number/head"
+git pull "%original_repo_url%" "pull/%pull_request_number%/head"
 
-# Step 4: Resolve conflicts (if any)
-# Handle conflicts manually in the code editor
+REM Step 4: Resolve conflicts (if any)
+REM Handle conflicts manually in the code editor
 
-# Step 5: Commit the changes
-read -p "Enter a commit message for the merge: " commit_message
+REM Step 5: Commit the changes
+set /p commit_message="Enter a commit message for the merge: "
 git add .
-git commit -m "$commit_message"
+git commit -m "%commit_message%"
 
-# Step 6: Push the changes to your fork
+REM Step 6: Push the changes to your fork
 git push origin pr-branch
 
-# Step 7: Create a pull request in your fork
-echo "To complete the process, go to your fork's repository on GitHub"
-echo "and create a pull request for the 'pr-branch' branch."
+REM Step 7: Create a pull request in your fork
+echo.
+echo To complete the process, go to your fork's repository on GitHub
+echo and create a pull request for the 'pr-branch' branch.
